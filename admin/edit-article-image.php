a5ad411f0a57e66776574afc0232f7a101553afb
Dani Matuko <matuko305@gmail.com>
2023-04-06 13:47:37 +0300

    restrict file type

--- admin/edit-article-image.php
+++ admin/edit-article-image.php
@@ -20,36 +20,46 @@ if (isset($_GET['id'])) {
     exit("id not supplied,aricle not found");
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-
-    echo '<pre>', var_dump($_FILES), '</pre>';
-
+    // var_dump($_FILES);
 
     try {
+
         if (empty($_FILES)) {
-            throw new Exception("Invalid upload");
+            throw new Exception('Invalid upload');
         }
 
-        if ($_FILES['file']['size'] > 1000000) {
-            throw new Exception('The max file size is 1MB');
-        }
-        $code = $_FILES['file']['error'];
-
-
-        switch ($code) {
+        switch ($_FILES['file']['error']) {
             case UPLOAD_ERR_OK:
                 break;
-            case UPLOAD_ERR_INI_SIZE:
-                throw new Exception("The uploaded file exceeds the upload_max_filesize directive in php.ini");
-                break;
+
             case UPLOAD_ERR_NO_FILE:
-                throw new Exception("No file was uploaded");
+                throw new Exception('No file uploaded');
+                break;
+
+            case UPLOAD_ERR_INI_SIZE:
+                throw new Exception('File is too large (from the server settings)');
                 break;
 
             default:
-                throw new Exception("Unknown upload error");
-                break;
+                throw new Exception('An error occurred');
+        }
+
+        // Restrict the file size
+        if ($_FILES['file']['size'] > 9000000) {
+
+            throw new Exception('File is too large');
+        }
+
+        $mime_types = ['image/gif', 'image/png', 'image/jpeg'];
+
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime_type = finfo_file($finfo, $_FILES['file']['tmp_name']);
+
+        if (!in_array($mime_type, $mime_types)) {
+
+            throw new Exception('Invalid file type');
         }
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -65,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <h1 class="display-3 mb-5 text-center">Edit article image</h1>
 
 
-    <form method="POST" enctype="multipart/form-data" novalidate class="w-50 m-auto">
+    <form method="POST" enctype="multipart/form-data" class="w-50 m-auto">
         <div class="mb-3">
             <label for="file" class="form-label">Image upload</label>
             <input type="file" class="form-control" id="file" name="file" value="<?= htmlspecialchars($article->image); ?>">
