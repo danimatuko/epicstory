6c76ba7dbbec0fb4c9941164f8b23d2b51abf7ed
Dani Matuko <matuko305@gmail.com>
2023-04-09 15:46:52 +0300

    delete file from the file system after removing the path form the db

--- admin/edit-article-image.php
+++ admin/edit-article-image.php
@@ -1,5 +1,4 @@
 <?php
-
 require '../includes/init.php';
 
 Auth::requireLogin();
@@ -19,80 +18,16 @@ if (isset($_GET['id'])) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo '<pre>', var_dump($_FILES), '</pre>';
-    exit;
 
     try {
+        $imageUploader = new ImageUploader($_FILES['file']);
 
-        if (empty($_FILES)) {
-            throw new Exception('Invalid upload');
-        }
-
-        switch ($_FILES['file']['error']) {
-            case UPLOAD_ERR_OK:
-                break;
-
-            case UPLOAD_ERR_NO_FILE:
-                throw new Exception('No file uploaded');
-                break;
-
-            case UPLOAD_ERR_INI_SIZE:
-                throw new Exception('File is too large (from the server settings)');
-                break;
-
-            default:
-                throw new Exception('An error occurred');
-        }
-
-        // Restrict the file size
-        if ($_FILES['file']['size'] > 3000000) {
-            throw new Exception('File is too large');
-        }
-
-        $mime_types = ['image/gif', 'image/png', 'image/jpeg'];
-
-        $finfo = finfo_open(FILEINFO_MIME_TYPE);
-        $mime_type = finfo_file($finfo, $_FILES['file']['tmp_name']);
-
-        if (!in_array($mime_type, $mime_types)) {
-            throw new Exception('Invalid file type');
-        }
-
-        $pathinfo = pathinfo($_FILES['file']['name']);
-        // sanitizse filename and create path to upload
-        $base = $pathinfo['filename'];
-        // Replace any characters that aren't letters, numbers, underscores or hyphens with an underscore
-        $base = preg_replace('/[^a-zA-Z0-9_-]/', "_", $base);
-
-        $filename = $base . "." . $pathinfo['extension'];
-
-        $destination = "../uploads/$filename";
-
-        // Add a numeric suffix to the filename to avoid overwriting existing files
-        $i = 1;
-
-        while (file_exists($destination)) {
-            $filename = $base . "-$i." . $pathinfo['extension'];
-            $destination = "../uploads/$filename";
-            $i++;
-        }
-
-        // upload file
-        $file_uploaded = move_uploaded_file($_FILES['file']['tmp_name'], $destination);
-
-        if ($file_uploaded) {
-            $previous_image_path = $article->image_path;
-            // set new image path
-            $pathHasChanged = $article->setImagePath($conn, $filename);
-            // remove the previous image after new image update
-            if ($pathHasChanged && $previous_image_path) {
-                unlink("../uploads/$previous_image_path");
-            }
-
-            header("Location: /admin/article.php?id={$article->id}");
-        } else {
-            throw new Exception('Unable to move uploaded file');
-        }
+        $imageUploader->uploadErrorCheck();
+        $imageUploader->checkSize();
+        $imageUploader->checkType();
+        $imageUploader->sanitizseFileName();
+        $imageUploader->setUniqueName();
+        $imageUploader->upload($conn, $article);
     } catch (Exception $e) {
         $error =  $e->getMessage();
     }
@@ -110,8 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <h1 class="display-3 mb-5 text-center">Edit article image</h1>
 
-    <form method="POST" action="image-uploader.php?id=<?= $article->id ?>" enctype="multipart/form-data"
-        class="w-50 m-auto mb-1">
+    <form method="POST" enctype="multipart/form-data" class="w-50 m-auto mb-1">
         <div class="mb-3">
             <label for="file" class="form-label">Image upload</label>
             <input type="file" class="form-control" id="file" name="file"
