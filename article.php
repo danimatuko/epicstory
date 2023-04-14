253784bcdad9a854d36b878b0109c13ec39993c4
Dani Matuko <matuko305@gmail.com>
2023-04-14 22:58:09 +0300

    only show article if it's been published

--- article.php
+++ article.php
@@ -8,7 +8,7 @@ $db = new Database();
 $conn = $db->getConn();
 
 if (isset($_GET['id'])) {
-    $article = Article::getWithCategories($conn, $_GET['id']);
+    $article = Article::getWithCategories($conn, $_GET['id'], true);
 } else {
     $article = null;
 }
@@ -21,29 +21,29 @@ if (isset($_GET['id'])) {
 <div class="w-75 m-auto">
 
     <a href="index.php">Back</a>
-
     <?php if (($article)) : ?>
-        <article class="py-3">
-            <h1 class="mb-3"><?= htmlspecialchars($article[0]['title']); ?></h1>
-            <!-- Categories -->
-            <?php if ($article[0]['category_name']) : ?>
-                <div>
-                    <strong>Categories:</strong>
-                    <?php foreach ($article as $a) : ?>
-                        <?= htmlspecialchars($a['category_name']) ?>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif ?>
+    <article class="py-3">
+        <small class="text-muted"><?= 'Published at: ' . $article[0]['published_at'] ?></small>
+        <h1 class="mb-3"><?= htmlspecialchars($article[0]['title']); ?></h1>
+        <!-- Categories -->
+        <?php if ($article[0]['category_name']) : ?>
+        <div>
+            <strong>Categories:</strong>
+            <?php foreach ($article as $a) : ?>
+            <?= htmlspecialchars($a['category_name']) ?>
+            <?php endforeach; ?>
+        </div>
+        <?php endif ?>
 
-            <?php if ($article[0]['image_path']) : ?>
-                <img src="/uploads/<?= htmlspecialchars($article[0]['image_path']); ?>" class="img-fluid mb-3" alt="">
-            <?php endif ?>
+        <?php if ($article[0]['image_path']) : ?>
+        <img src="/uploads/<?= htmlspecialchars($article[0]['image_path']); ?>" class="img-fluid mb-3" alt="">
+        <?php endif ?>
 
-            <p class="fs-5"><?= htmlspecialchars($article[0]['content']); ?></p>
-        </article>
+        <p class="fs-5"><?= htmlspecialchars($article[0]['content']); ?></p>
+    </article>
 
     <?php else : ?>
-        <p>No article found.</p>
+    <p>No article found.</p>
     <?php endif; ?>
 
 </div>
--- classes/Article.php
+++ classes/Article.php
@@ -223,7 +223,7 @@ class Article {
      *
      * @return array The article data with categories
      */
-    public static function getWithCategories($conn, $id) {
+    public static function getWithCategories($conn, $id, $only_published = false) {
         $sql = "SELECT article.*, category.name as category_name
                 FROM article
                 LEFT JOIN article_category
@@ -231,6 +231,10 @@ class Article {
                 LEFT JOIN category
                 ON article_category.category_id = category.id
                 WHERE article.id = :id";
+
+        if ($only_published) {
+            $sql .= " AND article.published_at IS NOT NULL";
+        }
 
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
