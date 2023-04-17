1976e5831a09851bdf6ec6545c483773d76c1dba
Dani Matuko <matuko305@gmail.com>
2023-04-17 12:50:19 +0300

    add screenshot

--- article.php
+++ article.php
@@ -23,36 +23,36 @@ if (isset($_GET['id'])) {
 
             <a href="index.php">Back</a>
             <?php if (($article)) : ?>
-            <article class="py-3">
-                <time datetime="<?= $article[0]['published_at']; ?>">
-                    <?php
+                <article class="py-3">
+                    <time datetime="<?= $article[0]['published_at']; ?>">
+                        <?php
                         $datetime = new DateTime($article[0]['published_at']);
                         echo $datetime->format("j F Y");
                         ?>
-                </time>
-                <h1 class="mb-3 d-block"><?= htmlspecialchars($article[0]['title']); ?></h1>
-                <!-- Categories -->
-                <div class="d-block">
-                    <?php if ($article[0]['category_name']) : ?>
-                    <div>
-                        <strong>Categories:</strong>
-                        <?php foreach ($article as $a) : ?>
-                        <div class="badge bg-secondary text-capitalize">
-                            <?= htmlspecialchars($a['category_name']) ?>
-                        </div> <?php endforeach; ?>
+                    </time>
+                    <h1 class="mb-3 d-block"><?= htmlspecialchars($article[0]['title']); ?></h1>
+                    <!-- Categories -->
+                    <div class="d-block">
+                        <?php if ($article[0]['category_name']) : ?>
+                            <div>
+                                <strong>Categories:</strong>
+                                <?php foreach ($article as $a) : ?>
+                                    <div class="badge bg-secondary text-capitalize">
+                                        <?= htmlspecialchars($a['category_name']) ?>
+                                    </div> <?php endforeach; ?>
+                            </div>
+                        <?php endif ?>
                     </div>
+
+                    <?php if ($article[0]['image_path']) : ?>
+                        <img src="/uploads/<?= htmlspecialchars($article[0]['image_path']); ?>" class="img-fluid mb-3" alt="">
                     <?php endif ?>
-                </div>
 
-                <?php if ($article[0]['image_path']) : ?>
-                <img src="/uploads/<?= htmlspecialchars($article[0]['image_path']); ?>" class="img-fluid mb-3" alt="">
-                <?php endif ?>
-
-                <p class="fs-5"><?= htmlspecialchars($article[0]['content']); ?></p>
-            </article>
+                    <p class="fs-5 py-5"><?= htmlspecialchars($article[0]['content']); ?></p>
+                </article>
 
             <?php else : ?>
-            <p>No article found.</p>
+                <p>No article found.</p>
             <?php endif; ?>
 
         </div>
